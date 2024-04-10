c996d483f818733b97440deadd1190c7bc85e803
reubenloma <reubensiama@lomatechnology.com>
2024-04-10 12:13:46 +0700

    redirect in funeral

--- app/Filament/Resources/FuneralResource/Pages/CreateFuneral.php
+++ app/Filament/Resources/FuneralResource/Pages/CreateFuneral.php
@@ -3,10 +3,14 @@
 namespace App\Filament\Resources\FuneralResource\Pages;
 
 use App\Filament\Resources\FuneralResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateFuneral extends CreateRecord
 {
     protected static string $resource = FuneralResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
