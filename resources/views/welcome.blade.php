94095ee688c1597cebba3c1b210c4420738d3f07
reubenloma <reubensiama@lomatechnology.com>
2024-04-11 11:10:07 +0700

    rename home page

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-@section('carousel')
-    <div class="px-4 md:px-10">
-        @include('layouts.carousel')
-    </div>
-@endsection
-@section('content')
-    This is content
-@endsection
