9e17939464c419494ad522f5d66d0035515dbc59
reuben <reuben154.infinity@gmail.com>
2024-05-14 14:37:04 +0700

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-gray-100 dark:bg-gray-900 mt-10">
     <div class="w-full mx-auto p-4 md:py-8">
-        <div class="sm:flex sm:items-center sm:justify-center">
+        <div class="flex items-center justify-center">
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
                     <a href="#" class="hover:underline me-4 md:me-6">About Us</a>
