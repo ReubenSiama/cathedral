3b14839192a04cb1477567b02becfd4109894732
reuben <reuben154.infinity@gmail.com>
2024-06-17 17:31:07 +0700

    logo

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,14 +5,27 @@
           md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700
           dark:hover:text-white md:dark:hover:bg-transparent'
 @endphp
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <div class="hidden md:flex flex-wrap justify-center items-center mx-auto max-w-screen-xl p-4">
+    <a href="{{ Route('home') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="{{ asset('images/new_logo.png') }}" alt="Logo" class="h-10">
+    </a>
+    {{-- <div class="flex items-center space-x-6 rtl:space-x-reverse">
+      <span class="text-sm">
+        Kulikawn, Aizawl
+        <br>
+        Mizoram, 796015
+      </span>
+    </div> --}}
+  </div>
+</nav>
+
 <nav class="bg-white border-gray-200 dark:bg-gray-900
-shadow-md dark:border-gray-700 fixed top-0 inset-x-0 z-50
+shadow-md dark:border-gray-700 inset-x-0 z-50
 ">
-  <div class="container flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Christ The King Cathedral
-        </span>
+  <div class="container flex flex-wrap items-center justify-between md:justify-center mx-auto p-4">
+    <a href="/" class="flex md:hidden items-center space-x-3 rtl:space-x-reverse">
+      <img src="{{ asset('images/new_logo.png') }}" alt="Logo" class="h-8">
     </a>
     <button data-collapse-toggle="navbar-default"
     type="button" class="
@@ -42,19 +55,28 @@ shadow-md dark:border-gray-700 fixed top-0 inset-x-0 z-50
           <a href="{{ route('home') }}" class="{{ Route::is('home') ? $activeClass : $inactiveClass }}" aria-current="page">Home</a>
         </li>
         <li>
-          <a href="#" class="{{ Route::is('clergy') ? $activeClass : $inactiveClass }}">Clergy & Religious</a>
+          <a href="{{ route('home') }}" class="{{ Route::is('home') ? $activeClass : $inactiveClass }}" aria-current="page">Bishops & Priests</a>
         </li>
         <li>
-          <a href="#" class="{{ Route::is('ppc') ? $activeClass : $inactiveClass }}">Parish Pastoral Council</a>
+          <a href="#" class="{{ Route::is('clergy') ? $activeClass : $inactiveClass }}">Religious & Catechists</a>
+        </li>
+        <li>
+          <a href="#" class="{{ Route::is('ppc') ? $activeClass : $inactiveClass }}">Stations</a>
         </li>
         <li>
           <a href="#" class="{{ Route::is('associations') ? $activeClass : $inactiveClass }}">Associations</a>
         </li>
         <li>
-          <a href="{{ route('gallery') }}" class="{{ Route::is('gallery') ? $activeClass : $inactiveClass }}">Gallery</a>
+          <a href="{{ route('gallery') }}" class="{{ Route::is('gallery') ? $activeClass : $inactiveClass }}">Parish Pastoral Council</a>
+        </li>
+        <li>
+          <a href="{{ route('gallery') }}" class="{{ Route::is('gallery') ? $activeClass : $inactiveClass }}">Institutions & Others</a>
         </li>
         <li>
           <a href="#" class="{{ Route::is('about.us') ? $activeClass : $inactiveClass }}">About Us</a>
+        </li>
+        <li>
+          <a href="{{ route('gallery') }}" class="{{ Route::is('gallery') ? $activeClass : $inactiveClass }}">Publications</a>
         </li>
       </ul>
     </div>
