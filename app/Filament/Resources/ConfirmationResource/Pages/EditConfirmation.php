19b496f0404dfaa0549defdd25f9fe2efeaf8582
reuben <reuben154.infinity@gmail.com>
2024-07-26 10:05:48 +0700

    redirect url

--- app/Filament/Resources/ConfirmationResource/Pages/EditConfirmation.php
+++ app/Filament/Resources/ConfirmationResource/Pages/EditConfirmation.php
@@ -19,9 +19,4 @@ class EditConfirmation extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
-
-    protected function getRedirectUrl(): string
-    {
-        return $this->getResource()::getUrl('index');
-    }
 }
