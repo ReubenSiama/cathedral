87b033773864d04a080388d468d5fd8de4511d08
ReubenSiama <reubenlalhmunsiama@gmail.com>
2024-07-26 20:20:13 +0700

    change how I set locale

--- app/Livewire/SwitchLanguage.php
+++ app/Livewire/SwitchLanguage.php
@@ -18,10 +18,8 @@ class SwitchLanguage extends Component
 
     public function switchLanguage($locale)
     {
-        session()->put('locale', $locale);
-        info('From Livewire: '.session()->get('locale'));
+        session(['locale' => $locale]);
         app()->setLocale($locale);
-        info('After setLocale:'.app()->getLocale());
 
         return redirect(request()->header('Referer'));
     }
